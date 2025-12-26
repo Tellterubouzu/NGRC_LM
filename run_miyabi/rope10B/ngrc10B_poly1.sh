54dbef65bcf666ec8b4f8b4db2f947f3666ad084
Dusan <dusan.eftimov@uttorracars.com>
2025-12-26 20:43:58 +0900

    changed the job script for rope

--- run_miyabi/rope10B/ngrc10B_poly1.sh
+++ run_miyabi/rope10B/ngrc10B_poly1.sh
@@ -24,6 +24,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate esn
 
 cd ../../src
-python ngrc_rope_lm_cross_low_rank.py --local_batch_size 192 --learning_rate 5e-4 --seq_len 512 --ngrc_d_model 2048 --ngrc_lag 10 --ngrc_poly_degree 1 ---ngrc_cross_mode "random" --log_grad --total_tokens 10e9 --wandb_run_name "NGRC_LM_ROPE(113.51M_10BT_random_lr5e-4_d2048_poly3_bs198)"
+python ngrc_rope_lm_cross_low_rank.py --local_batch_size 192 --learning_rate 5e-4 --seq_len 512 --ngrc_d_model 2048 --ngrc_lag 10 --ngrc_poly_degree 1 --ngrc_cross_mode "random" --total_tokens 10e9 --wandb_run_name "NGRC_LM_ROPE(113.51M_10BT_random_lr5e-4_d2048_poly3_bs198)"
 
-python ngrc_rope_lm_cross_low_rank.py --local_batch_size 192 --learning_rate 1e-3 --seq_len 512 --ngrc_d_model 2048 --ngrc_lag 10 --ngrc_poly_degree 1 ---ngrc_cross_mode "random" --log_grad --total_tokens 10e9 --wandb_run_name "NGRC_LM_ROPE(113.51M_10BT_random_lr1e-3_d2048_poly1_bs198)"
+python ngrc_rope_lm_cross_low_rank.py --local_batch_size 192 --learning_rate 1e-3 --seq_len 512 --ngrc_d_model 2048 --ngrc_lag 10 --ngrc_poly_degree 1 --ngrc_cross_mode "random" --total_tokens 10e9 --wandb_run_name "NGRC_LM_ROPE(113.51M_10BT_random_lr1e-3_d2048_poly1_bs198)"
